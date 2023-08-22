e7531e671a9f6d448a620063a49149c0275b56f8
Rodolfo Santana <rodolfo@fasters.com.br>
2023-08-21 22:10:24 -0300

    feat: add spring boot initializr

---
+++ src/main/java/dev/kitani/helloworld/HelloWorldApplication.java
@@ -0,0 +1,14 @@
+package dev.kitani.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication( exclude = {SecurityAutoConfiguration.class})
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+}
